b7f6659669743fcc8d80c81003fa3f80d3cc01d5
Sujan Basnet <itssujan167@gmail.com>
2024-05-09 20:55:00 +0545

    ArtcurialSpider completed

--- watchscrapy/watchscrapy/spiders/ArtcurialSpider.py
+++ watchscrapy/watchscrapy/spiders/ArtcurialSpider.py
@@ -19,8 +19,8 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 class ArtcurialSpider(scrapy.Spider):
     name = "artcurialSpider"
     allowed_domains = ["www.artcurial.com"]
-    start_urls = [
-        'https://www.artcurial.com/en/sales/vente-fr-3484-modern-vintage-watches-online']
+    # start_urls = [
+    #     'https://www.artcurial.com/en/sales/vente-fr-3484-modern-vintage-watches-online']
 
     def __init__(self, url='', job='', *args, **kwargs):
         super(ArtcurialSpider, self).__init__(*args, **kwargs)
@@ -33,69 +33,68 @@ class ArtcurialSpider(scrapy.Spider):
         # SELENIUM_CHROMEDRIVER_PATH = setup.chromedriver
         options = webdriver.ChromeOptions()
         options.add_argument("start-maximized")
-        options.add_argument('headless')
+        # options.add_argument('headless')
         browser = webdriver.Chrome(options=options)
         browser.set_window_size(1440, 900)
         return browser
 
     def start_requests(self):
-        # Provide a complete URL with the scheme (e.g., "http://" or "https://")
         start_urls = [
             'https://www.artcurial.com/en/sales/vente-fr-3484-modern-vintage-watches-online']
-        browser = self.sel_configuration()
-        logged_in = self.login(browser)
+        self.browser = self.sel_configuration()
+        self.login(self.browser)
         for url in start_urls:
-            yield scrapy.Request(url=url, callback=self.parse, meta={'browser': browser})
-
+            yield scrapy.Request(url=url, callback=self.parse, meta={"browser": self.browser})
 
     def parse(self, response):
+        print("inside parse")
         logging.warn(
             "ArtcurialSpider; msg=Spider started;url= %s", response.url)
         try:
-            name = response.xpath(
-                '//*[@id="app"]/div/main/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[1]/h3/text()').extract()
+            self.browser.get(response.url)
+            print(f"\nself.browser.get({response.url})\n")
+            time.sleep(15)
+            name = self.browser.find_element(By.XPATH,
+                                             '//*[@id="app"]/div/main/div/div[1]/div/div[1]/div/div/div[2]/div[1]/div/div/div/h3').text
             # 3
-            date = response.xpath(
-                '//*[@id="app"]/div/main/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div/table/tbody/tr[1]/td[2]/div[2]/div/a/span/text()').extract()
+            date_string = self.browser.find_element(By.XPATH,
+                                                    '//*[@id="app"]/div/main/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div/table/tbody/tr[1]/td[2]/div[2]/div/a/span').text
+            full_date_and_time = date_string.split(" at ")
+            full_date = full_date_and_time[0]
+            
+            partial_date = datetime.strptime(full_date, '%B %d, %Y')
+            date = partial_date.strftime('%b %d,%Y')
+
             # 4
             location = "Online"
-
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Content-Type': 'application/json',
-                "X-Algolia-Api-Key": "3349c930e83dcf3e4de1de65bc7af214",
-                "X-Algolia-Application-Id": "R0QMYZZKST"
-
-            }
-            body = {
-                "query": "", "filters": "saleObjectId:vente-fr-3484-modern-vintage-watches-online", "hitsPerPage": 200, "numericFilters": ""
-            }
-            url = "https://r0qmyzzkst-dsn.algolia.net/1/indexes/items/query?x-algolia-agent=Algolia%20for%20JavaScript%20(4.15.0)%3B%20Browser"
-            resp = requests.post(url, headers=headers, json=body)
-            
-            # resp = requests.get(new_url)
-            respj = json.loads(resp.text)            
-
             auction_url = response.url
-            print(f"\nrespnose_url:: {response.url}\n\n")
-
             # Scrape another page using Selenium
             browser = response.meta.get('browser')
-            z = browser.get(response.url)
-            print(f'\n\n ----------- z:: {z} \n\n\n\n\n--------')
-            time.sleep(10)
-            all_lots = browser.find_elements(By.XPATH, '//*[@id="app"]/div/main/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div/table/tbody/tr[2]/td[1]/h5/text()')
+            try:
+                browser.get(response.url)
+                print("\nPage loaded succesfully\n")
+            except Exception as e:
+                print(f'\nFailed to load page: {e}\n')
+
+            time.sleep(20)
+
+            all_lots_elements = browser.find_elements(
+                By.XPATH, '//*[@id="app"]/div/main/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div/table/tbody/tr[2]/td[1]/h5')
+
+            # Extracting text content from each element in the list
+            all_lots = [element.text for element in all_lots_elements]
+            all_lots = int(all_lots[0].split()[0])
+
             print(f"\n\n-- all_lots:: {all_lots}\n\n")
 
-            total_lots = len(all_lots)
-            logging.warn("ArtcurialSpider; msg=Total Lots: %s;url= %s", len(
-                all_lots), response.url)
-            for lots in all_lots:
-                url_segment = lots.xpath(
-                    'div[@class="mosaic__item-img"]/a/@href').extract()[0]
-                url = "https://" + self.allowed_domains[0] + url_segment
-                lot_number = lots.xpath(
-                    'div[@class="mosaic__item-infos"]/div[1]/p/a/span/text()').extract()[0]
+            total_lots = all_lots
+            print(f"\n\n-- total_lots:: {total_lots}\n\n")
+            # =====================================
+            logging.warn("ArtcurialSpider; msg=Total Lots: %s;url= %s",
+                         all_lots, response.url)
+            for index in range(all_lots):
+                url = response.url + "/lots/"+str(index+1)
+                lot_number = index+1
                 items = {'name': name, 'date': date, 'location': location,
                          'lot_number': lot_number, 'auction_url': auction_url, 'lots': total_lots}
                 yield scrapy.Request(url, callback=self.parse_url, meta=items)
@@ -111,6 +110,7 @@ class ArtcurialSpider(scrapy.Spider):
 
     def parse_url(self, response):
         item = WatchItem()
+
         try:
             # 1 HouseName
             house_name = 2
@@ -122,8 +122,9 @@ class ArtcurialSpider(scrapy.Spider):
 
             # 3 Date
             date = response.meta.get("date")
-            item["date"] = datetime.strptime(
-                date.strip(), '%d %B %Y').strftime('%b %d,%Y')
+            item['date'] = date
+            # item["date"] = datetime.strptime(
+            # date.strip(), '%d %B %Y').strftime('%b %d,%Y')
 
             # 4 Location
             location = response.meta.get("location")
@@ -131,67 +132,86 @@ class ArtcurialSpider(scrapy.Spider):
 
             # 5 Lot Number
             lot_number = response.meta.get("lot_number")
-            item["lot"] = re.findall(r'\d+', lot_number)[0]
 
-            product_visuals = response.xpath('//div[@class="product-visuals"]')
+            item["lot"] = re.findall(r'\d+', str(lot_number))[0]
 
-            # 6 Images
-            images_info = product_visuals.xpath(
-                'div[1]/ul/li/img/@src').extract()
+            self.browser.get(response.url)
+            time.sleep(10)
 
-            images = "https://" + \
-                self.allowed_domains[0] + "/" + images_info[0]
+            # Find all elements matching the XPath
+            image_element = self.browser.find_element(
+                By.XPATH, '/html/body/div/div/div/div/main/div/section/div[1]/div[2]/div[2]/div[1]/div/div/div[1]/div/div[2]/div[1]/img')
+            images = image_element.get_attribute("src")
 
-            item["images"] = images
+            # Split the URL based on "?"
+            image = images.split("?")
 
-            strong_info = product_visuals.xpath(
-                'div[@class="product-visuals__description description-tablet-landscape-up"]/strong/text()').extract()
-            if not strong_info:
-                strong_info = product_visuals.xpath(
-                    'div[@class="product-visuals__description description-tablet-landscape-up"]/p/strong/text()').extract()
+            # Take the first part which contains the URL without query parameters
+            image = image[0]
+            item["images"] = image
+            print(f'\nresponse.url:: {response.url}')
 
-            # 7 title
-            if len(strong_info) > 1 and "Estimation" not in strong_info[1]:
-                title = strong_info[0] + " " + strong_info[1]
-            else:
-                title = strong_info[0]
-            item["title"] = title
+            title = self.browser.find_elements(
+                By.XPATH, '//*[@id="app"]/div/main/div/section/div[1]/div[1]/ul/li[1]/div/span')
+            title = [element.text for element in title]
+            item["title"] = " ".join(title)
 
             # 8 Description
-            description = product_visuals.xpath(
-                'div[@class="product-visuals__description description-tablet-landscape-up"]').extract()[0]
-            item["description"] = description
+            description = self.browser.find_elements(
+                By.XPATH, '//*[@id="app"]/div/main/div/section/div[1]/div[2]/div[2]/div[2]/div/div/div')
+            description = [element.text for element in description]
+            description = " ".join(description)
 
-            estimation_info = strong_info[-1].strip().replace(
-                "Estimation", "").strip().split("-")
-            sold_pre_info = product_visuals.xpath(
-                'div[@class="product-visuals__description description-tablet-landscape-up"]/span/text()').extract()
+            # Remove all single quotes
+            text_without_single_quotes = description.replace("'", "")
 
-            if len(strong_info) > 1 and "Estimation" not in strong_info[-1]:
-                est_min_price = est_max_price = 0
-                lot_currency = sold_pre_info[0].split()[-1]
-            else:
-                est_min_price = estimation_info[0].strip().split(
-                    " ")[-1].replace(",", "")
-                est_max_price = estimation_info[1].strip().split(" ")[
-                    0].replace(",", "")
-                lot_currency = estimation_info[1].strip().split(" ")[-1]
+            # Remove all double quotes
+            text_without_quotes = text_without_single_quotes.replace('"', '')
+            item["description"] = text_without_quotes
+
+            estimation_info = self.browser.find_elements(
+                By.XPATH, '//*[@id="app"]/div/main/div/section/div[1]/div[2]/div[3]/div/div[1]/div/div[2]/div[2]/span')
+            estimation_info = [element.text for element in estimation_info]
 
             # 9 Lot Currency
-            item["lot_currency"] = lot_currency
+            item["lot_currency"] = "€"
             # 10 Est min Price
-            item["est_min_price"] = est_min_price
+            # Define regular expression pattern to match numeric values
+            pattern = r'\d+'
+
+            # Initialize variables for min and max prices
+            min_price = None
+            max_price = None
+
+            # Iterate through each string in the list
+            for currency in estimation_info:
+                # Extract numeric values using regular expression
+                matches = re.findall(pattern, currency)
+
+                # Check if matches are found
+                if matches:
+                    # Convert the extracted numeric values to integers
+                    values = [int(match) for match in matches]
+
+                    # Assign the values to min_price and max_price
+                    if len(values) == 1:
+                        min_price = values[0]
+                    elif len(values) >= 2:
+                        min_price = min(values)
+                        max_price = max(values)
+
+            item["est_min_price"] = min_price
 
             # 11 Est max Price
-            item["est_max_price"] = est_max_price
+            item["est_max_price"] = max_price
 
             # 12 sold
             sold = sold_price = 0
-            if sold_pre_info:
-                sold_info = sold_pre_info[0].split(" ")
-                if sold_info[0] == "Sold":
-                    sold = 1
-                    sold_price = sold_info[1].replace(",", "")
+            # if sold_pre_info:
+            #     sold_info = sold_pre_info[0].split(" ")
+            #     if sold_info[0] == "Sold":
+            #         sold = 1
+            #         sold_price = sold_info[1].replace(",", "")
 
             item["sold"] = sold
             # 13 sold_price
